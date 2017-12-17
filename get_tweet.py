e3f75811feb00eb2096e831daf1490b7e51d3cc0
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2017-12-17 17:29:08 +0900

    twitterAPI用のライブラリをtweepyに変更 指定したtweetの取得する部分書いた アプリの認証が上手くいかない

--- get_tweet.py
+++ get_tweet.py
@@ -1,5 +1,10 @@
-#from request_oauthlib import OAuth1Session
+import tweepy
 import settings
 
 def get_tweet():
-    print(CONSUMER_KEY)
+    auth = tweepy.OAuthHandler(settings.CONSUMER_KEY,settings.CONSUMER_SECRET)
+    auth.set_access_token(settings.ACCESS_TOKEN,settings.ACCESS_TOKEN_SECRET)
+    api=tweepy.API(auth)
+
+    id="162076119531667457"
+    api.get_status(id)
