42f59bba790eec3dd0b2e0f1b2e8e5028deb44da
rimocon <ttyd4343-ttyd@yahoo.co.jp>
2017-12-23 01:36:12 +0900

    ツイートできた

--- reply.py
+++ reply.py
@@ -1,7 +1,9 @@
 import tweepy
 import settings
 
-def get_tweet():
+def reply_tweet(average):
     auth = tweepy.OAuthHandler(settings.CONSUMER_KEY,settings.CONSUMER_SECRET)
     auth.set_access_token(settings.ACCESS_TOKEN,settings.ACCESS_TOKEN_SECRET)
     api=tweepy.API(auth)
+
+    api.update_status(status='うおおおおおおおおおおpythonからツイートできたああああああああああああ')
