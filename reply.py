cc34c8be47964a460e7cd409fda8f4b001963faa
rimocon <ttyd4343-ttyd@yahoo.co.jp>
2017-12-23 00:43:12 +0900

    first commit

---
+++ reply.py
@@ -0,0 +1,7 @@
+import tweepy
+import settings
+
+def get_tweet():
+    auth = tweepy.OAuthHandler(settings.CONSUMER_KEY,settings.CONSUMER_SECRET)
+    auth.set_access_token(settings.ACCESS_TOKEN,settings.ACCESS_TOKEN_SECRET)
+    api=tweepy.API(auth)
