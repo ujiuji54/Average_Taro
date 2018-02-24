4a87733a86a533238906fa4faede1947842d1e7d
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-02-24 21:20:42 +0900

    main部完成

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from get_tweet import get_tweet
 from get_average import get_average
+from reply import reply
 
-scores=get_average()
-get_tweet()
+import tweepy
+import settings
+auth = tweepy.OAuthHandler(settings.CONSUMER_KEY,settings.CONSUMER_SECRET)
+auth.set_access_token(settings.ACCESS_TOKEN,settings.ACCESS_TOKEN_SECRET)
+api=tweepy.API(auth)
+
+while True:
+    for tweet_id in get_tweet():
+        reply(tweet_id,get_average(api.get_status(tweet_id).text))
