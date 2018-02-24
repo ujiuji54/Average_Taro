2787a8edf97d033e7a998daf59406ef1f5d28126
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-02-24 20:41:05 +0900

    reply()完成

--- reply.py
+++ reply.py
@@ -1,9 +1,11 @@
 import tweepy
 import settings
 
-def reply_tweet(average):
+def reply(tweet_id,average):
     auth = tweepy.OAuthHandler(settings.CONSUMER_KEY,settings.CONSUMER_SECRET)
     auth.set_access_token(settings.ACCESS_TOKEN,settings.ACCESS_TOKEN_SECRET)
     api=tweepy.API(auth)
 
-    api.update_status(status='うおおおおおおおおおおpythonからツイートできたああああああああああああ')
+    reply_tweet = "@" + str(api.get_status(tweet_id).user.screen_name) +" "+ str(average)
+    print(reply_tweet)
+    api.update_status(status=reply_tweet, in_repry_to_status_id=tweet_id)
